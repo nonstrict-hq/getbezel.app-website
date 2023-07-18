ef270b0e586d76465a5ff5490f5a00893d9036fc
Mathijs Kadijk <mkadijk@gmail.com>
2023-07-18 22:33:34 +0200

    Initial setup and first draft landingpage

---
+++ Sources/BezelWebsite/Theme/Bezel.swift
@@ -0,0 +1,52 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mathijs Kadijk on 25/01/2023.
+//
+
+import Foundation
+import Publish
+import Plot
+
+extension Theme where Site == BezelWebsite {
+    static var bezel: Self {
+        Theme(
+            htmlFactory: BezelWebsiteHTMLFactory(),
+            resourcePaths: []
+        )
+    }
+
+    private struct BezelWebsiteHTMLFactory: HTMLFactory {
+        func makeIndexHTML(for index: Publish.Index, context: Publish.PublishingContext<BezelWebsite>) throws -> Plot.HTML {
+            var indexWithImage = index
+            indexWithImage.imagePath = context.site.imagePath
+            return HTML(
+                .head(for: indexWithImage, on: context.site),
+                .body(
+                    .component(HeroAlt())
+                )
+            )
+        }
+
+        func makeSectionHTML(for section: Publish.Section<BezelWebsite>, context: Publish.PublishingContext<BezelWebsite>) throws -> Plot.HTML {
+            HTML(.body(.text("TODO")))
+        }
+        
+        func makeItemHTML(for item: Publish.Item<BezelWebsite>, context: Publish.PublishingContext<BezelWebsite>) throws -> Plot.HTML {
+            HTML(.body(.text("TODO")))
+        }
+        
+        func makePageHTML(for page: Publish.Page, context: Publish.PublishingContext<BezelWebsite>) throws -> Plot.HTML {
+            HTML(.body(.text("TODO")))
+        }
+        
+        func makeTagListHTML(for page: Publish.TagListPage, context: Publish.PublishingContext<BezelWebsite>) throws -> Plot.HTML? {
+            HTML(.body(.text("TODO")))
+        }
+        
+        func makeTagDetailsHTML(for page: Publish.TagDetailsPage, context: Publish.PublishingContext<BezelWebsite>) throws -> Plot.HTML? {
+            HTML(.body(.text("TODO")))
+        }
+    }
+}
