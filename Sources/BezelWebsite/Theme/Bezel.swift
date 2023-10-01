99dfa4be019773570fba85b1babc41ed10668345
Tom Lokhorst <tom@lokhorst.eu>
2023-10-01 17:52:29 +0200

    Add robots noindex to two pagess

--- Sources/BezelWebsite/Theme/Bezel.swift
+++ Sources/BezelWebsite/Theme/Bezel.swift
@@ -58,7 +58,8 @@ extension Theme where Site == BezelWebsite {
                 )
             case "thank-you-for-trying-bezel":
                 return HTML(
-                    .head(for: pageWithImage, on: context.site),
+                    .head(for: pageWithImage, on: context.site, nodes: [
+                        .meta(.attribute(named: "name", value: "robots"), .attribute(named: "content", value: "noindex"))]),
                     .body(
                         .attribute(named: "class", value: "bg-gray-900"),
                         .component(Header()),
@@ -68,7 +69,8 @@ extension Theme where Site == BezelWebsite {
                 )
             case "subscribed":
                 return HTML(
-                    .head(for: pageWithImage, on: context.site),
+                    .head(for: pageWithImage, on: context.site, nodes: [
+                        .meta(.attribute(named: "name", value: "robots"), .attribute(named: "content", value: "noindex"))]),
                     .body(
                         .attribute(named: "class", value: "bg-gray-900"),
                         .component(Header()),
@@ -88,5 +90,54 @@ extension Theme where Site == BezelWebsite {
         func makeTagDetailsHTML(for page: Publish.TagDetailsPage, context: Publish.PublishingContext<BezelWebsite>) throws -> Plot.HTML? {
             HTML(.body(.text("Not found")))
         }
+    }
+}
+
+public extension Node where Context == HTML.DocumentContext {
+
+    // Copy of head function from Plot, with extra node parameter
+    static func head<T: Website>(
+        for location: Location,
+        on site: T,
+        titleSeparator: String = " | ",
+        stylesheetPaths: [Path] = ["/styles.css"],
+        rssFeedPath: Path? = .defaultForRSSFeed,
+        rssFeedTitle: String? = nil,
+        nodes: [Node<HTML.HeadContext>] = []
+    ) -> Node {
+        var title = location.title
+
+        if title.isEmpty {
+            title = site.name
+        } else {
+            title.append(titleSeparator + site.name)
+        }
+
+        var description = location.description
+
+        if description.isEmpty {
+            description = site.description
+        }
+
+        return .head(
+            .encoding(.utf8),
+            .siteName(site.name),
+            .url(site.url(for: location)),
+            .title(title),
+            .description(description),
+            .twitterCardType(location.imagePath == nil ? .summary : .summaryLargeImage),
+            .forEach(stylesheetPaths, { .stylesheet($0) }),
+            .viewport(.accordingToDevice),
+            .unwrap(site.favicon, { .favicon($0) }),
+            .unwrap(rssFeedPath, { path in
+                let title = rssFeedTitle ?? "Subscribe to \(site.name)"
+                return .rssFeedLink(path.absoluteString, title: title)
+            }),
+            .unwrap(location.imagePath ?? site.imagePath, { path in
+                let url = site.url(for: path)
+                return .socialImageLink(url)
+            }),
+            .forEach(nodes, { $0 })
+        )
     }
 }
